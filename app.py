d11fcacc82e0b1fa8ca5e335017d91090762ea74
Jerome Baude <jba@mymac.local>
2021-09-08 18:12:53 +0200

    updating app.py

--- app.py
+++ app.py
@@ -6,10 +6,14 @@ app = Flask(__name__)
 
 # list of cat images
 images = [
-    "https://media.giphy.com/media/F3O8iAVrKgiR6QtgnE/giphy.gif",
-    "https://media.giphy.com/media/A7ZbCuv0fJ0POGucwV/giphy.gif",
-    "https://media.giphy.com/media/dOJt6XZlQw8qQ/giphy.gif",
-    "https://media.giphy.com/media/gVBDitqGpVvRC/giphy.gif"
+    "https://raw.githubusercontent.com/jeromebaude/my-tweet-app-lacework/master/Pictures/la110.png",
+    "https://raw.githubusercontent.com/jeromebaude/my-tweet-app-lacework/master/Pictures/la111witoutperson.png",
+    "https://raw.githubusercontent.com/jeromebaude/my-tweet-app-lacework/master/Pictures/PNGFILE-01.png",
+    "https://raw.githubusercontent.com/jeromebaude/my-tweet-app-lacework/master/Pictures/NoRulesShirtPNG.png",
+    "https://raw.githubusercontent.com/jeromebaude/my-tweet-app-lacework/master/Pictures/D151rulesoptwithoutman.png",
+    "https://raw.githubusercontent.com/jeromebaude/my-tweet-app-lacework/master/Pictures/D151rulesopt.png",
+    "https://raw.githubusercontent.com/jeromebaude/my-tweet-app-lacework/master/Pictures/D13R1wp.png",
+    "https://raw.githubusercontent.com/jeromebaude/my-tweet-app-lacework/master/Pictures/D136Beachoopwithoutman.png"
 ]
 
 @app.route('/')
