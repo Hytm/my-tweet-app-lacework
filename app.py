61d116b713f3a7fa2f6c6afbf5fe3b09419e30c4
Andreas Wilke <andiwilke@gmail.com>
2021-09-20 18:06:26 +0200

    adding lacework scan test

--- app.py
+++ app.py
@@ -18,5 +18,11 @@ def index():
     hostname = platform.node()
     return render_template('index.html', url=url, hostname=hostname)
 
+@app.route('/lacework')
+def lacework():
+    url = random.choice(images)
+    hostname = platform.node()
+    return render_template('lacework-scan.html', url=url, hostname=hostname)
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
